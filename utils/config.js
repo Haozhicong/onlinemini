59588cf34174a43714a5df8bed15c65e70b6310d
haozhicong <1754269628@qq.com>
2019-07-31 15:34:11 +0800

    配置文件

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,14 @@
+// 线上
+// module.exports = {
+//     host: "localhost",
+//     user: "root",
+//     password: "Hzc@950917",
+//     database: "huaiancar"
+// }
+// 线下
 module.exports = {
     host: "localhost",
     user: "root",
-    password: "Hzc@950917",
+    password: "123456",
     database: "huaiancar"
 }
