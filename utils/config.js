1d604750d9374d5d8db7f4246f446944b72b9ce4
haozhicong <1754269628@qq.com>
2019-07-31 15:37:47 +0800

    地址修改

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,14 @@
 // 线上
-// module.exports = {
-//     host: "localhost",
-//     user: "root",
-//     password: "Hzc@950917",
-//     database: "huaiancar"
-// }
-// 线下
 module.exports = {
     host: "localhost",
     user: "root",
-    password: "123456",
+    password: "Hzc@950917",
     database: "huaiancar"
 }
+// 线下
+// module.exports = {
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     database: "huaiancar"
+// }
